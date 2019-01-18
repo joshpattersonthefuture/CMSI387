5fffc48689adc339e9b78537216f1c6107903b63
jpatt <34358565+joshpattersonthefuture@users.noreply.github.com>
2019-01-17 18:57:35 -0800

    Initial blastoff

---
+++ Classwork/Classwork01/SayHello.c
@@ -0,0 +1,9 @@
+/* Author: Joshua Patterson *\
+/* Simple Hello, World Program in C *\
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char * argv[] ) {
+  printf("\n\n Hello, world! \n\n ");
+}

a4fead8e4a7cbb144242e11fb02878ae83aa638c
Josh Patterson <34358565+ookarooka@users.noreply.github.com>
2019-04-21 15:15:37 -0700

    modified (works)

--- Homework/Homework02/BoundedBuffer.java
+++ Homework/Homework02/BoundedBuffer.java
@@ -1,60 +1,68 @@
 //Joshua Patterson & Alex Richardson
 // #3 & 4
+
+//define first BoundedBuffer
 public class BoundedBuffer {
     private int SIZE = 3;
     private Object[] buffer = new Object[SIZE]; // arbitrary size
     private int numOccupied = 0;
     private int firstOccupied = 0;
 
-    /*
-     * invariant: 0 <= numOccupied <= buffer.length 0 <= firstOccupied <
-     * buffer.length buffer[(firstOccupied + i) % buffer.length] contains the
-     * (i+1)th oldest entry, for all i such that 0 <= i < numOccupied
-     */
-public class Inserter extends Thread {
+
+//insert and retrieve for first BoundedBuffer
     public synchronized void insert(Object o) throws InterruptedException {
         while (numOccupied == buffer.length)
-            // wait for space
             wait();
         buffer[(firstOccupied + numOccupied) % buffer.length] = o;
         numOccupied++;
-        if (numOccupied == 0) {
             notifyAll();
-        }
     }
-}
 
-public class Retriever extends Thread {
     public synchronized Object retrieve() throws InterruptedException {
         while (numOccupied == 0)
-            // wait for data
             wait();
         Object retrieved = buffer[firstOccupied];
         buffer[firstOccupied] = null; // may help garbage collector
         firstOccupied = (firstOccupied + 1) % buffer.length;
         numOccupied--;
-        // in case any inserts are waiting for space, wake them
-        if (numOccupied == SIZE) {
             notifyAll();
-        }
-        return retrieved;
-    }
+              return retrieved;
+   }
 }
+//insert and retrieve for second BoundedBuffer
+class NewBoundedBuffer  {
+    private int SIZE = 3;
+    private Object[] buffer = new Object[SIZE];
+    public int numOccupied = 0;
+    public int firstOccupied = 0;
 
-    public void print() {
-        System.out.print("Buffer Contains: ");
-        for (Object a : buffer) {
-            System.out.print(a + " ");
-        }
-        System.out.print("Num Occupied: " + numOccupied);
-        System.out.println("First Occupied: " + firstOccupied);
+//define new BoundedBuffer
+    public NewBoundedBuffer() {
+        this.buffer = new Object[SIZE];
+        this.numOccupied = 0;
+        this.firstOccupied = 0;
     }
 
-
-public static void main (String args[]) {
-   Retriever r = new BoundedBuffer().new Retriever();
-   Inserter i = new BoundedBuffer().new Inserter();
-   r.start();
-   i.start();
+  public synchronized void insert(Object o) throws InterruptedException {
+      while (numOccupied == buffer.length)
+          wait();
+      buffer[(firstOccupied + numOccupied) % buffer.length] = o;
+      numOccupied++;
+       if (numOccupied == 0) {
+          notifyAll();
+        }     
   }
+
+  public synchronized Object retrieve() throws InterruptedException {
+      while (numOccupied == 0)
+          wait();
+      Object retrieved = buffer[firstOccupied];
+      buffer[firstOccupied] = null; // may help garbage collector
+      firstOccupied = (firstOccupied + 1) % buffer.length;
+      numOccupied--;
+      if (numOccupied == SIZE) {
+       notifyAll();
+      }
+   return retrieved;
+ }
 }

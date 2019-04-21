2a29b131dce5370788e4e54b6cd1064d120948cd
Josh Patterson <34358565+ookarooka@users.noreply.github.com>
2019-04-21 15:16:29 -0700

    modified (works)

--- Homework/Homework02/BoundedBufferTest.java
+++ Homework/Homework02/BoundedBufferTest.java
@@ -1,45 +1,25 @@
-public class BoundedBufferTest {
-    BoundedBuffer buff;
+//Joshua Patterson & Alex Richardson
+// #3 & 4
+public class BoundedBufferTest extends BoundedBuffer {
+   /**
+   * the main method which calls all of the test methods in the class
+   * @param args String[] array containing command line parameters
+   * @return void
+   */
+   public static void main ( String [] args ) throws Exception {
+      BoundedBuffer bufferOne = new BoundedBuffer();
+      NewBoundedBuffer bufferTwo = new NewBoundedBuffer();
+      try{
+        bufferOne.insert("hello world");
+        bufferTwo.insert("goodbye world");
+        System.out.println("Inserted");
 
-    public void test_full_buffer() {
-        buff = new BoundedBuffer();
-        
-        Thread t1 = new Thread();
-        try {
-            buff.insert(t1);
-        } catch (Exception e) {
-            System.out.println("Cannot insert thread to buffer");
+        Object o = new Object();
+        bufferOne.insert(o);
+        bufferOne.retrieve();
+        System.out.println("Retrieved");
+      } catch (Exception e) {
+        System.out.println(e);
         }
-        buff.print();
-
-        Thread t2 = new Thread();
-        try {
-            buff.insert(t2);
-        } catch (Exception e) {
-            System.out.println("Cannot insert thread to buffer");
-        }
-        buff.print();
-
-        Thread t3 = new Thread();
-        try {
-            buff.insert(t3);
-        } catch (Exception e) {
-            System.out.println("Cannot insert thread to buffer");
-        }
-        buff.print();
-
-        Thread t4 = new Thread();
-        try {
-            buff.insert(t4);
-        } catch (Exception e) {
-            System.out.println("Cannot insert thread to buffer");
-        }
-        buff.print();
-    }
-
-
-    public static void main(String[] args) {
-        BoundedBufferTest test = new BoundedBufferTest();
-        test.test_full_buffer();
-    }
+   }     
 }
